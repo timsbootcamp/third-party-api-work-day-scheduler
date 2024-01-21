ccfeddf063b369adf22198fb54f83581d4ebba86
Tim Anand <timanand@gmail.com>
2024-01-21 15:50:39 +0000

    Update save events to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 const localStorage_WorkDaySched = "work-day-scheduler";
 
 let businessHours = [
-  "9am", 
-  "10am", 
-  "11am", 
-  "12am", 
-  "1pm", 
-  "2pm", 
-  "3pm", 
-  "4pm", 
-  "5pm",
-  "6pm",
-  "7pm"
+  {"id":"9am", "24hrTime":9},
+  {"id":"10am", "24hrTime":10},
+  {"id":"11am", "24hrTime":11},
+  {"id":"12pm", "24hrTime":12},
+  {"id":"1pm", "24hrTime":13},
+  {"id":"2pm", "24hrTime":14},
+  {"id":"3pm", "24hrTime":15},
+  {"id":"4pm", "24hrTime":16},
+  {"id":"5pm", "24hrTime":17},
 ]   
 
 let calendarData = [];
@@ -22,6 +20,7 @@ const dayWeek = today.format("dddd"); // returns day of week (eg. 'Friday')
 const currentMonth = today.format('MMMM'); // eg. January
 const currentDay = today.format('D');  // eg. 19 (ie. day of month)
 
+
 // using Jquery get id of currentDay
 var dateEl = $("#currentDay");
 
@@ -29,29 +28,35 @@ let todaysDate_ddmmyyyy = today.format("DD/MM/YYYY");
 
 
 
+// this is only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
 $(document).ready(function() {
 
   // update date using dateEl via Jquery
   dateEl.text(dayWeek + ", " + currentMonth + " " + currentDay);
 
-
-
+  
   for (var i = 0; i < businessHours.length; i++) {
     //var $time = businessHours[i].displayTime;
-    var $time = businessHours[i];
+    var $time = businessHours[i].id;
 
     // Add new div with class: row
     var $row = $("<div>").addClass("row");
 
     // Add new div with class: col-md-1 time-block
     var $timeBlock = $("<div>").addClass("col-md-1 time-block time-block");   
-    $timeBlock.append("<p>" + businessHours[i] + "</p>");
+    $timeBlock.append("<p>" + businessHours[i].id + "</p>");
+   
+    var $textareaContainer = $("<textarea>").addClass("col-md-10").attr({ id: $time, rows: 2, cols: 100, placeholder: "\u200b" });
 
-    var $textareaContainer = $("<textarea>").addClass("col-md-10").attr({ id: $time, rows: 2, cols: 100 });
+    var label = $('<label>', {
+      'for': $time,
+      'text': ''
+    });
 
+    
     var $saveButton = $("<div>")
       .addClass("col-md-1 saveBtn center-container")
-      .attr("id", $time)
+      .attr("_id", $time)
       .append("<i class='fas fa-save'></i>")
       .on("click", saveButtonClick);
 
@@ -59,9 +64,11 @@ $(document).ready(function() {
       // Append all data into one variable
     var $rowTimeLine =  $row
       .append($('<div>').addClass('time-block-container')) // Add space at top
+      .append(label)
       .append($timeBlock)
-      .append($textareaContainer)
+      .append($textareaContainer)    
       .append($saveButton);
+
 
 
     // Select an element with the ID "root-timeblock" from HTML and store in variable 'root'
@@ -71,11 +78,10 @@ $(document).ready(function() {
     root.append($rowTimeLine);
   }
 
-
-
+  
+  readFromLocalStorage();
   displayTimeBlockColors();
 
-  
 })
 
 
@@ -84,9 +90,10 @@ $(document).ready(function() {
 
 // Event trigerred when user clicks on saveButton
 function saveButtonClick() {
-  var $buttonId   = $(this).attr("id");
+  var $buttonId   = $(this).attr("_id");
   var $textEnteredbyUser = $("#" + $buttonId).val();
-  updateCalendarData("20/01/2024", $buttonId, $textEnteredbyUser);
+
+  updateCalendarData($buttonId, $textEnteredbyUser);
 }
 
 
@@ -97,10 +104,13 @@ function displayTimeBlockColors() {
   var textAreas_dataEntry = $(".col-md-10");
 
   for (var i = 0; i < textAreas_dataEntry.length; i++) {
-    if (currentHour > businessHours[i]) {
+    
+    let busHour = parseInt(businessHours[i]["24hrTime"], 10)
+    
+    if (currentHour > busHour) {
       $(textAreas_dataEntry[i]).addClass("past");
     }
-    else if (currentHour == businessHours[i]) {
+    else if (currentHour == busHour) {
       $(textAreas_dataEntry[i]).addClass("present");
     }
     else {
@@ -110,45 +120,35 @@ function displayTimeBlockColors() {
 }
 
 
-
-
-function checkCalendarStructure() {
+function checkCalendarStructureForReset() {
 
   // read from local storage
   var storedData = localStorage.getItem(localStorage_WorkDaySched);
+
   if (storedData)
   {
     // compare today's date with last stored in local storage
-    calendarData = JSON.parse(storedData);
+    let calendarData = JSON.parse(storedData);
 
     // if dates differ then clear it out ready for new day  
     if (calendarData[0].date != todaysDate_ddmmyyyy )
     {
       // clear local storage
       clearWorkDayScheduler();
-      
-      // initialise calendar for today    
       initialiseCalendarStructure();    
     }
-  }
-  else
-  {
-    console.log("");
-    // this has never been run before so initialise calendar structure
-    initialiseCalendarStructure();
   }
 }
 
 
 
 function initialiseCalendarStructure() {
-
+  
   //let propertiesString = '{"date":' + '"20/01/2024"';
   let propertiesString = '{"date":' + '"' + todaysDate_ddmmyyyy + '"';
-
-  
+ 
   for (let i = 0; i < businessHours.length; i++) {
-    propertiesString = propertiesString + ", "  + '"' + businessHours[i] + '"' + ':' + '"' + "" + '"';
+    propertiesString = propertiesString + ", "  + '"' + businessHours[i].id + '"' + ':' + '"' + "" + '"';
   }
 
   propertiesString = propertiesString + "}";
@@ -161,13 +161,14 @@ function initialiseCalendarStructure() {
   // Adding the object to the array 
   calendarData.push(propertiesObject);
   writeToLocalStorage();
-
 }
 
 
 
 
 function readFromLocalStorage() {
+
+  checkCalendarStructureForReset();
 
   var storedData = localStorage.getItem(localStorage_WorkDaySched);
 
@@ -177,16 +178,17 @@ function readFromLocalStorage() {
     calendarData = JSON.parse(storedData);
 
     for (var i = 0; i < businessHours.length; i++) {
-      let $data = calendarData[0][businessHours[i]]
-      $("#" + businessHours[i]).append($data);
-    }       
+      let $data = calendarData[0][businessHours[i].id]
+      $("#" + businessHours[i].id).append($data);
+    }         
+  }
+  else {
+    initialiseCalendarStructure(); 
   }
 }
 
 
 function writeToLocalStorage() {
-    
-  checkCalendarStructure();
   
   // Converts the calendarData array to the JSON notation that the value represents
   var jsonCalendarData = JSON.stringify(calendarData);
@@ -196,12 +198,14 @@ function writeToLocalStorage() {
 }
 
 
-function updateCalendarData(dateEvent, timeEvent, userData) {
-  let item = calendarData.find(element => element.date === dateEvent);
+function updateCalendarData(timeEvent, userData) {
+
+  checkCalendarStructureForReset();
+
+  let item = calendarData.find(element => element.date === todaysDate_ddmmyyyy);
   item[timeEvent] = userData;
 
   writeToLocalStorage();
- 
 }
 
 
@@ -210,14 +214,4 @@ function clearWorkDayScheduler() {
   // Clear storage based on key 
   localStorage.removeItem(localStorage_WorkDaySched);
 }
-
-
-
-// this is only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
-$(document).ready(function () {
-  // checkCalendarStructure();
-  //readFromLocalStorage();
-})
-
-
 
