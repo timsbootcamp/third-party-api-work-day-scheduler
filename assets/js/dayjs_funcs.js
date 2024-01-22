018ec6d77e3660da35ec5611224bc9f4373ae994
Tim Anand <timanand@gmail.com>
2024-01-22 10:30:10 +0000

    Add day with suffix

--- assets/js/dayjs_funcs.js
+++ assets/js/dayjs_funcs.js
@@ -12,7 +12,7 @@ function getDateForTopOfWebPage() {
   const currentMonth = dayjslib.format('MMMM'); // eg. January
   const currentDay = dayjslib.format('D');  // eg. 19 (ie. day of month)
  
-  return dayWeek + ", " + currentMonth + " " + currentDay
+  return dayWeek + ", " + currentMonth + " " + currentDay + addDaySuffix(currentDay)
 }
 
 
@@ -26,5 +26,30 @@ function getCurrentHour() {
   return dayjs().format('HH');
 }
 
+// Takes a parameter variable, 'day' and returns with the suffix ("st", "nd", "rd", or "th")
+function addDaySuffix(day) {
 
+  // Convert parameter variable, 'day' to a string
+  const dayString = day.toString();
+
+  // Extract last character and convert it back to a number
+  const lastDigit = Number(dayString.charAt(dayString.length - 1));
+
+  // Handle 11th, 12th, and 13th
+  if (day >= 11 && day <= 13) {
+    return 'th';
+  }
+  else {
+    switch (lastDigit) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  }
+}
 
